65c67acc0dc9a5e5df8d3170ce9b5ef3ef2401f6
Jon Tejada <tejjon@gmail.com>
2015-12-05 12:54:48 -0800

    delete comments

--- day5/part2_script.js
+++ day5/part2_script.js
@@ -29,37 +29,6 @@ fs.readFile('data.txt', 'utf8', function(err,data){
 			}
 		}
 
-
-		// //vowel test
-		// var hasThreeVowels = false;
-		// var vowelCount = 0;
-		// for (j=0; j<input.length; j++) {
-		// 	if (input[j] === 'a' || input[j] === 'e' || input[j] === 'i' || input[j] === 'o' || input[j] === 'u') {
-		// 		vowelCount++;
-		// 	}
-		// }
-		// if (vowelCount > 2) {
-		// 	hasThreeVowels = true;
-		// }
-
-		// //twice test
-		// var hasLetterTwice = false;
-		// for (j=0; j<input.length; j++) {
-		// 	if (input[j] === input[j+1]) {
-		// 		hasLetterTwice = true;
-		// 	}
-		// }
-
-		// //bad str test
-		// var noBadStrings = true;
-		// var badStrings = ['ab', 'cd', 'pq', 'xy'];
-		// for (j=0; j<input.length; j++) {
-		// 	var twoLetters = input[j] + input[j+1];
-		// 	if (twoLetters === 'ab' ||twoLetters === 'cd' ||twoLetters === 'pq' ||twoLetters === 'xy') {
-		// 		noBadStrings = false;
-		// 	}
-		// }
-
 		//add nice strings
 		if (hasPairRepeat && hasLetterTwice) {
 			niceStrCount++;
