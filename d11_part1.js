ebc116a55c6bed7441ed55cd20ed9b4243df25cf
Jon Tejada <tejjon@gmail.com>
2015-12-12 15:06:12 -0800

    finished day 11 part 1

--- d11_part1.js
+++ d11_part1.js
@@ -12,68 +12,11 @@
 // o = 111
 // l = 108
 
-// valid password:
-// straight of three chars, no invalids, two pair sets
+// valid password: straight of three chars, no invalids, two pair sets
 
-// var myInput = 'vzbxkghb';
-// var testInput = 'abzz';
-
-// function nextLetter(inputLetter) {
-// 	var output = String.fromCharCode(inputLetter.charCodeAt() + 1);
-// 	if (output === '{') {
-// 		output = 'a';
-// 	}
-// 	return output;
-// }
-
-// // console.log(nextLetter('b'));
-// // console.log(nextLetter('z'));
-
-// 	// for (var i = input.length - 1 ; i >= 0 ; i--) {
-// 	// 	output = input.charCodeAt(i) + output;
-// 	// }
-// 	// var output = String.fromCharCode(input.charCodeAt(input.length - 1) + 1);
-
-
-// function nextPassword(inputStr) {
-// 	//last char
-// 	// var output = nextLetter(inputStr[inputStr.length - 1]);
-// 	//rest of password
-// 	// var n = 1;
-// 	for (var i = 0 ; i < inputStr.length ; i++) {
-// 		if (output[output.length - (i+1)] === 'a') {
-// 			// output = nextLetter(inputStr[inputStr.length - (i+1)])
-// 		}
-// 	}
-
-// 	while (output[output.length-n] === 'a') {
-// 		n++;
-// 		output = nextLetter(inputStr[inputStr.length - n]) + output;
-// 	}
-
-
-// 	var output = '';
-
-// 	for (var i = inputStr.length -1 ; i >=0 ; i--) {
-// 		if (inputStr[i] === 'z') {
-// 			output = nextLetter(inputStr[i]) + output;
-
-// 		}
-// 	}
-
-
-// 	return output;
-// }
-
-
-// // console.log(nextPassword(myInput));
-// console.log(nextPassword(testInput));
-
-
-
-
-var myInput = 'vzbxkihb';
-var testInput = 'abaz';
+var realInput = 'vzbxkghb';
+var testInput1 = 'vzbxkihb';
+var testInput2 = 'abaz';
 
 function convertInput(inputStr) {
 	var workingArr = [];
@@ -105,6 +48,7 @@ function nextPass(inputStr) {
 	return outputStr;
 }
 
+
 function hasInvalid(inputStr) {
 	var output = false;
 	for (var i = 0 ; i < inputStr.length ; i++) {
@@ -115,19 +59,53 @@ function hasInvalid(inputStr) {
 	return output;
 }
 
+function hasStraight(inputStr) {
+	var output = false;
+	var inputArr = convertInput(inputStr);
+	if (inputArr[0] === inputArr[1] + 1 && inputArr[0] === inputArr[2] + 2) {
+		output = true;
+	}
+	if (inputArr[1] === inputArr[2] + 1 && inputArr[1] === inputArr[3] + 2) {
+		output = true;
+	}
+	if (inputArr[2] === inputArr[3] + 1 && inputArr[2] === inputArr[4] + 2) {
+		output = true;
+	}
+	if (inputArr[3] === inputArr[4] + 1 && inputArr[3] === inputArr[5] + 2) {
+		output = true;
+	}
+	if (inputArr[4] === inputArr[5] + 1 && inputArr[4] === inputArr[6] + 2) {
+		output = true;
+	}
+	if (inputArr[5] === inputArr[6] + 1 && inputArr[5] === inputArr[7] + 2) {
+		output = true;
+	}
+	return output;
+
+}
+
+function hasTwoPair(inputStr) {
+	var output = false;
+	var inputArr = convertInput(inputStr);
+	var pairs = 0;
+	for (var i = 0; i < inputArr.length; i++) {
+		if (inputArr[i] === inputArr[i+1]) {
+			pairs++;
+			i++;
+		}
+	}
+	if (pairs > 1) {
+		output = true;
+	}
+	return output;
+}
+
 function nextValidPass(inputStr) {
 	var output = nextPass(inputStr);
-	while (hasInvalid(output)) {
+	while (hasInvalid(output) || !hasStraight(output) || !hasTwoPair(output)) {
 		output = nextPass(output);
 	}
 	return output;
 }
 
-console.log(nextPass(myInput));
-
-
-
-
-
-
-
+console.log(nextValidPass(realInput));
