1f7a7e65c72629a844fdab3fdec61ae31ad5b02d
Jon Tejada <tejjon@gmail.com>
2015-12-14 09:54:25 -0800

    more day13 work

--- day13/day13_part1.js
+++ day13/day13_part1.js
@@ -27,19 +27,84 @@ fs.readFile('data.txt', 'utf8', function(err,data) {
 		arr[k] = elem.split(' ');
 	});
 	// console.log(happyArr);
-
+	
 	var totalHappy = [];
+
 	allPermutations.forEach(function(permElem,n,arr) {
+		var seat1 = permElem[0];
+		var seat2 = permElem[1];
+		var seat3 = permElem[2];
+		var seat4 = permElem[3];
+		var seat5 = permElem[4];
+		var seat6 = permElem[5];
+		var seat7 = permElem[6];
+		var seat8 = permElem[7];
+		var count = 0;
+
+		console.log(happyArr.length);
+
+
 		for (var j = 0; j< happyArr.length; j++) {
-			var firstName = happyArr[0][0];
-			var secondName = happyArr[10][0];
+			console.log(j + happyArr[j][0][0]);
+			var firstName = happyArr[j][0][0];
+			var secondName = happyArr[j][10][0];
 			var valueStr = '';
-			if (happyArr[2] === 'lose') {
+			if (happyArr[j][2] === 'lose') {
 				valueStr += '-';
 			}
-			valueStr += happyArr[3];
-			console.log(firstName+secondName+valueStr);
+			valueStr += happyArr[j][3];
+
+			if (seat1 === firstName && seat2 === secondName) {
+				count += Number(valueStr);
+			}
+			if (seat2 === firstName && seat3 === secondName) {
+				count += Number(valueStr);
+			}
+			if (seat3 === firstName && seat4 === secondName) {
+				count += Number(valueStr);
+			}
+			if (seat4 === firstName && seat5 === secondName) {
+				count += Number(valueStr);
+			}
+			if (seat5 === firstName && seat6 === secondName) {
+				count += Number(valueStr);
+			}
+			if (seat6 === firstName && seat7 === secondName) {
+				count += Number(valueStr);
+			}
+			if (seat7 === firstName && seat8 === secondName) {
+				count += Number(valueStr);
+			}
+			if (seat8 === firstName && seat1 === secondName) {
+				count += Number(valueStr);
+			}
+			if (seat1 === secondName && seat2 === firstName) {
+				count += Number(valueStr);
+			}
+			if (seat2 === secondName && seat3 === firstName) {
+				count += Number(valueStr);
+			}
+			if (seat3 === secondName && seat4 === firstName) {
+				count += Number(valueStr);
+			}
+			if (seat4 === secondName && seat5 === firstName) {
+				count += Number(valueStr);
+			}
+			if (seat5 === secondName && seat6 === firstName) {
+				count += Number(valueStr);
+			}
+			if (seat6 === secondName && seat7 === firstName) {
+				count += Number(valueStr);
+			}
+			if (seat7 === secondName && seat8 === firstName) {
+				count += Number(valueStr);
+			}
+			if (seat8 === secondName && seat1 === firstName) {
+				count += Number(valueStr);
+			}
 		}
+		console.log(count);
+		happyArr.push(count);
 	});
 });
 
